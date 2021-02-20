1566b0856c65f0e407fe659ecfbfd591a01fa510
= <s_necib@enst.dz>
2021-02-20 17:57:36 +0100

    after .ignore file commit

--- miniprojet/src/main/java/com/kabli/managementsystem/Launch.java
+++ miniprojet/src/main/java/com/kabli/managementsystem/Launch.java
@@ -1,2 +1,19 @@
-package com.kabli.managementsystem;public class Launch {
+package com.kabli.managementsystem;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Launch extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene scene = new Scene(new Label("hello"));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 }
